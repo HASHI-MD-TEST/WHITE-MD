4a60417b516efaffbaeedef13222d41f2566dc19
HASHI-MD-TEST <hasithnirmal501@gmail.com>
2025-07-28 15:56:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "JhIx1ILS#IYe_ouHmB2RM48JeN2LVck7n2xTRlFghjjETDxztZv0",
+SESSION_ID: process.env.SESSION_ID || "mIkBRTyK#4-HH67Q2_CWf6ApKWaWAhlmp2umIMpuRZ9UqoAfrmuQ",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
